f1bca8c0b63ddf1722bb7ab4da6037eb0b7fe20b
huijintian <wu750316@163.com>
2017-11-25 14:22:25 +0800

    add transaction

---
+++ src/main/java/org/smart/service/CustomerService.java
@@ -0,0 +1,7 @@
+package org.smart.service;
+
+/**
+ * Created by mengtian on 2017/11/25
+ */
+public interface CustomerService {
+}
---
+++ src/main/java/org/smart/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,7 @@
+package org.smart.service.impl;
+
+/**
+ * Created by mengtian on 2017/11/25
+ */
+public class CustomerServiceImpl {
+}
